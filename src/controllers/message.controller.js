2d146e22fc8c41632e3e687dcd2a9f0d72936ad5
zxinminaung-dev <146321635+zxinminaung-dev@users.noreply.github.com>
2024-07-01 18:46:25 +0630

    commit

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -4,7 +4,8 @@ const socketService = require('../services/base/socket.service')
 const router = express.Router()
 router.get('/send', (req,res)=>{
     let message = req.query.message
-    socketService.sendMessageToClients(message)
+    var ip = req.socket.remoteAddress
+    socketService.sendMessageToClients(message + " ("+ip+")")
     res.json({success:true}).status(200);
 })
 module.exports = router;
