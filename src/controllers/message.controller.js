05bd71dc5d48ba9d0314b0d11ba45f381e87d72f
zxinminaung-dev <146321635+zxinminaung-dev@users.noreply.github.com>
2024-07-01 19:04:39 +0630

    commit

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -2,9 +2,12 @@ const express = require('express')
 const Constant = require('../utils/constant')
 const socketService = require('../services/base/socket.service')
 const router = express.Router()
+
+//sending request message
 router.get('/send', (req,res)=>{
     let message = req.query.message
     socketService.sendMessageToClients(message)
     res.json({success:true}).status(200);
 })
+
 module.exports = router;
